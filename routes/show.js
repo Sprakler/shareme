88225c0247ef96a49e63f5ec1ae2ec086cc94ce9
Uday Kiran <kiranu941@gmail.com>
2020-09-11 10:27:25 +0530

    Modified Download link

--- routes/show.js
+++ routes/show.js
@@ -11,7 +11,7 @@ router.get('/:uuid',async (req, res) =>{
             uuid: file.uuid,
             filename: file.filename,
             fileSize: file.size,
-            downloadLink: `${process.env.APP_BASE_URL}/files/download/${file.uuid}`
+            downloadLink: `/files/download/${file.uuid}`
         })
     }catch(err) {
         return res.render('download',{ error:'Something went Wrong.'})
